f0af8b72dc9c912d523721e698e00031683750c1
Hakla <hakla0123456789@gmail.com>
2025-12-06 11:10:44 +0000

    Data Transformation

--- src/utils.py
+++ src/utils.py
@@ -60,3 +60,26 @@
 
 #     except Exception as e:
 #         raise CustomException(e, sys)
+
+
+
+
+import os
+import sys
+import nupy as np
+import pandas as pd
+import dill
+
+from src.Exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj, file_path)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+        
